4a75ea4d330d6a05951b3ad12fd4b86ce033cd37
Lars Rustand <rustand.lars@gmail.com>
2024-02-17 15:40:18 +0100

    Docstrings

--- keyboard.lisp
+++ keyboard.lisp
@@ -129,6 +129,7 @@
     (f7 . #xC8) (f8 . #xC9) (f9 . #xCA) (f10 . #xCB) (f11 . #xCC) (f12 . #xCD)
 
     ;; Shifted keys
+    ;; TODO: add keycodes directly
     (colon . (shft semi)) (undsc . (shft minus)) (dquot . (shft quot))
     (quest . (shft slash)) (pipe . (shft bslash)) (lt . (shft comma))
     (gt . (shft dot))
@@ -164,8 +165,8 @@
 ;; Utility functions
 ;; ------------------------------------------------------------------
 
-;; Return the biggest number in array
 (defun max-in-array (arr)
+  "Find the largest value in array"
   (let ((max (aref arr 0)))
     (dotimes (pos (length arr))
       (let ((val (aref arr pos)))
@@ -174,6 +175,7 @@
     max))
 
 (defun get-bit (byt b)
+  "Get bit number `b' from `byt'"
   (if (plusp (logand (ash 1 b) byt)) 1 0))
 
 
@@ -182,9 +184,9 @@
 ;; ------------------------------------------------------------------
 
 
-;; Make sure all entries in `*keycodes*' are valid,
-;; and that there are no keycode collisions.
 (defun validate-keycodes ()
+  "Make sure all entries in `*keycodes*' are valid, and that there
+are no keycode collisions."
   (dolist (key-a *keycodes*)
     (dolist (key-b *keycodes*)
       ;; Only compare key-a to keys before it
@@ -201,8 +203,8 @@
                   symbol-a symbol-b keycode-a))))))
 
 
-;; Make sure all `*keymaps*' and `*layout*' has the same geometry
 (defun validate-keymap ()
+  "Make sure all `*keymaps*' and `*layout*' have the same geometry"
   (dolist (km *keymaps*)
     (unless (= (length (strip-garbage km)) (length *layout*))
       (format t "WARNING: Keymap and layout has different length!")))
@@ -212,9 +214,9 @@
 ;; Keymap cleanup functions
 ;; ------------------------------------------------------------------
 
-;; Returns `t' if the element is "garbage" and should be
-;; dropped from the keymap or layout.
 (defun garbage? (elem)
+  "Returns `t' if the element is garbage and should be
+dropped from the keymap or layout."
   (or (eq '│ elem)
       (let ((string-elem (princ-to-string elem)))
         (or (search "─" string-elem)
@@ -230,8 +232,8 @@
 
 
 ;; TODO: Figure out why the long symbols are split
-;; Remove "comments" from keymap.
 (defun strip-garbage (keymap)
+  "Remove comments from keymap"
   (if (null keymap)
       keymap
     (let ((elem (car keymap)))
@@ -249,24 +251,27 @@
 
 
 (defun lookup-key (key)
+  "Lookup the keycode or special key bound to `key'"
   (let ((string-key (princ-to-string key)))
     (if (= 1 (length string-key))
         (char-code (char string-key 0))
       (or (cdr (assoc key *keycodes*)) 0))))
 
 
-;; Actually send the key
 (defun send-key (keycode pressed?)
+  "Send a key press/release for the given keycode"
   (if (= pressed? 0)
       (keyboard/release keycode)
     (keyboard/press keycode)))
 
 
 (defun activate-layer (layer)
+  "Activate a layer"
   (format t "Activating layer ~a~%" layer)
   (setf *layer* layer))
 
 (defun deactivate-layer (layer)
+  "Deactivate a layer"
   (format t "Deactivating layer ~a~%" layer)
   (setf *layer* 0))
 
@@ -300,8 +305,8 @@
 ;; Matrix scanning functions
 ;; ------------------------------------------------------------------
 
-;; Poll key matrix from slave over I2C and process events
 (defun process-slave-events ()
+  "Poll key matrix from slave over I2C and process events."
   (let* ((key-pos *cols*)
          (bytes (with-i2c (str *i2c-port* *slave-address* *rows*)
                   (list (read-byte str)
@@ -322,9 +327,9 @@
 
 
 
-;; Find which row+column is pressed,
-;; compare to previous state, and dispatch key events
 (defun process-events ()
+  "Find which row+column is pressed, compare to previous state,
+and dispatch key events."
   (let ((key-pos 0)) ;; This is the position in the flattened keymatrix
     (dolist (row-pin *row-pins*)
       ;; Enable row
@@ -349,9 +354,9 @@
 ;; Init functions
 ;; ------------------------------------------------------------------
 
-;; Use the `layout' to convert the visually laid out `keymap' into
-;; a matrix organized in rows and columns based on the eletrical layout
 (defun initialize-keymatrix ()
+  "Use `*layout*' to convert the visually laid out `*keymaps*' into
+a matrix organized in rows and columns based on the eletrical layout"
   (setq *keymatrix* (make-array (list (length *keymaps*) (+ 1 (max-in-array *layout*))) :initial-element nil))
   (dotimes (layer (length *keymaps*))
     (let ((keymap-pos 0))
@@ -360,7 +365,10 @@
           (setf (aref *keymatrix* layer matrix-pos) key))
         (incf keymap-pos)))))
 
+
 (defun init ()
+  "Set up IO pins, validate keymap and keycodes, initialize keyboard,
+and calculate global variables."
   ;; Set up IO pins
   (dolist (col-pin *col-pins*)
     (pinmode col-pin :input-pullup))
@@ -387,9 +395,10 @@
 ;; TODO: Make ulisp fork
 ;; TODO: Add dockerfile
 ;; TODO: Write short instructions for setup/installation
-;; TODO: Write docstrings
+;; TODO: Make makefile or similar
 
 (defun main ()
+  "Main function. Run as a keyboard in an infinite loop."
   (init)
   (loop
     (process-events)))
